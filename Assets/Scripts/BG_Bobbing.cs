cfdb22bcf7fce83e1ec443bfc3e4443c6e02a521
Ronald DeSilva <61198729+RonaldDeSilva@users.noreply.github.com>
2023-08-10 16:42:45 -0400

    Made Baby Beards Ship look like its bobbing

--- Assets/Scripts/BG_Bobbing.cs
+++ Assets/Scripts/BG_Bobbing.cs
@@ -4,30 +4,36 @@ using UnityEngine;
 
 public class BG_Bobbing : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private int dir;
+    private float dir;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         StartCoroutine("Up");
     }
 
     void Update()
     {
-        rb.velocity = new Vector2(0,dir);
+        transform.Translate(new Vector3(0, dir * Time.deltaTime));
     }
 
     IEnumerator Up()
     {
-        dir = 1;
-        yield return new WaitForSeconds(1f);
+        dir = 0.95f;
+        yield return new WaitForSeconds(0.5f);
+        dir = 0.45f;
+        yield return new WaitForSeconds(0.35f);
+        dir = 0.2f;
+        yield return new WaitForSeconds(0.15f);
         StartCoroutine("Down");
     }
 
     IEnumerator Down()
     {
-        dir = -1;
-        yield return new WaitForSeconds(1f);
+        dir = -0.95f;
+        yield return new WaitForSeconds(0.5f);
+        dir = -0.45f;
+        yield return new WaitForSeconds(0.35f);
+        dir = -0.2f;
+        yield return new WaitForSeconds(0.15f);
         StartCoroutine("Up");
     }
 }
