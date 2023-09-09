a84d5d8f77ff343164e75a760c1674822e3b5716
Ronald DeSilva <61198729+RonaldDeSilva@users.noreply.github.com>
2023-09-09 15:23:10 -0400

    Added Mysterious Stranger Graphics and fixed arm rotation script

--- Assets/Scripts/General Or Level/CopyRot.cs
+++ Assets/Scripts/General Or Level/CopyRot.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CopyRot : MonoBehaviour
 {
-    private GameObject thing;
+    private Transform thing;
+    public bool reverse;
+    private Transform rot;
     void Start()
     {
-        thing = transform.parent.GetChild(0).gameObject;
+        thing = transform.parent.GetChild(0).transform.GetChild(1);
+        rot = transform;
     }
 
     void Update()
     {
-        transform.rotation = thing.transform.rotation;
+        //transform.LookAt(thing, );
+        Vector3 diff = thing.position - rot.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
     }
 }
